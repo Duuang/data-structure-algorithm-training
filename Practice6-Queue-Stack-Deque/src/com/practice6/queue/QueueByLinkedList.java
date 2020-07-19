29a358676f63058f9b09053fc33848077e998043
cky <15201692553@126.com>
2020-07-20 00:31:26 +0800

    submit Queue& Stack implemented by linked list

---
+++ Practice6-Queue-Stack-Deque/src/com/practice6/queue/QueueByLinkedList.java
@@ -0,0 +1,56 @@
+package com.practice6.queue;
+
+import com.practice6.stack.StackByLinkedList;
+
+public class QueueByLinkedList {
+  
+  class ListNode {
+    int value;
+    ListNode next;
+    // ListNode prev;
+    public ListNode(int value) {
+      this.value = value;
+    }
+  }
+  private ListNode head;
+  private ListNode tail;
+  
+  // add at tail
+  public void add(int value) {
+    if (head == null) {
+      head = tail = new ListNode(value);
+    } else {
+      tail.next = new ListNode(value);
+      tail = tail.next;
+    }
+  }
+  
+  //remove at head
+  public Integer remove() {
+    if (head == null) {
+      return null;
+    }
+    ListNode prev = head;
+    head = head.next;
+    if (head == null) {
+      tail = null;
+    }
+    prev.next = null;  //good practice
+    return prev.value;
+  }
+  
+  public Integer peek() {
+    if (head == null)
+      return null;
+    return head.value;
+  }
+  
+  public static void main(String[] args) {
+    QueueByLinkedList test = new QueueByLinkedList();
+    test.add(1);
+    test.add(2);
+    test.add(3);
+    test.remove();
+    test.remove();
+  }
+}
---
+++ Practice6-Queue-Stack-Deque/src/com/practice6/stack/StackByLinkedList.java
@@ -0,0 +1,48 @@
+package com.practice6.stack;
+
+//use ListNode to implement stack (push, pop, peek)
+public class StackByLinkedList {
+  
+  public static void main(String[] args) {
+    StackByLinkedList test = new StackByLinkedList();
+    test.push(1);
+    test.push(2);
+    test.push(3);
+    test.pop();
+    test.pop();
+  }
+  
+  class ListNode {
+    int value;
+    ListNode next;
+    // ListNode prev;
+    public ListNode(int value) {
+      this.value = value;
+    }
+  }
+  private ListNode head;
+  
+  public Integer pop() {
+    if (head == null) {
+      return null;
+    }
+    ListNode popped = head;
+    head = head.next;
+    popped.next = null;   //good practice
+    return popped.value;
+  }
+  
+  public void push(int value) {
+    ListNode newNode = new ListNode(value);
+    newNode.next = head;
+    head = newNode;
+  }
+  
+  public Integer peek() {
+    if (head == null) {
+      return null;
+    }
+    return head.value;
+  }
+}
+
